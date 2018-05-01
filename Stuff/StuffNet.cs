c36b2dd1a7633375f0a5c38681691d3eaade9f05
michael berezin <mbearz@gmail.com>
2018-05-01 19:52:02 +0300

    firstCheckin

---
+++ Stuff/StuffNet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stuff
+{
+    public class StuffNet
+    {
+        string configKey = string.Empty;
+        public StuffNet()
+        {
+            configKey = ConfigurationManager.AppSettings["configKey"];
+        }
+
+
+        public string GetKey(string key)
+        {
+            return configKey + "_" + ConfigurationManager.AppSettings[key];
+        }
+    }
+}
---
+++ StuffCore/StuffCore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Configuration;
+
+namespace StuffCore
+{
+    public class StuffCore
+    {
+        string configKey = string.Empty;
+        public StuffCore()
+        {
+            configKey = ConfigurationManager.AppSettings["configKey"];
+        }
+
+
+        public string GetKey(string key)
+        {
+            return ConfigurationManager.AppSettings[key] + "_" + configKey;
+        }
+    }
+}
